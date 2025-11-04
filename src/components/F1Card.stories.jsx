d7dea79c7cb374f6729f885b940344c014c5ec8e
Monolixd <jakkridjantung@gmail.com>
2025-11-04 20:06:37 +0700

    style:Change Driver Profile Picture

--- src/components/F1Card.stories.jsx
+++ src/components/F1Card.stories.jsx
@@ -6,7 +6,7 @@ const verstappenData = {
   team: "Red Bull Racing",
   number: 1,
   imageUrl:
-    "https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/M/MAXVER01_Max_Verstappen/maxver01.png.transform/1col/image.png",
+    "https://img.redbull.com/images/c_crop,w_3840,h_1920,x_0,y_0/c_auto,w_1200,h_630/f_auto,q_auto/redbullcom/2024/11/24/nrqoxx9as35r5ry8ashm/max-verstapen-2024-f1-world-champion-four",
   stats: {
     wins: 54,
     poles: 38,
@@ -19,7 +19,7 @@ const hamiltonData = {
   team: "Mercedes AMG Petronas",
   number: 44,
   imageUrl:
-    "https://media.formula1.com/d_driver_fallback_image.png/content/dam/fom-website/drivers/L/LEWHAM01_Lewis_Hamilton/lewham01.png.transform/1col/image.png",
+    "https://images.ps-aws.com/c?url=https%3A%2F%2Fd3cm515ijfiu6w.cloudfront.net%2Fwp-content%2Fuploads%2F2025%2F10%2F30173348%2Flewis-hamilton-ferrari-f1-2025-mexican-grand-prix-1320x742.png",
   stats: {
     wins: 103,
     poles: 104,

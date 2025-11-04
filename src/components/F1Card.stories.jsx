3e620d1f2ec8752122319a3155d5a2dbb9e536bd
Monolixd <jakkridjantung@gmail.com>
2025-11-04 21:04:42 +0700

    feat:import data from f1data.js

--- src/components/F1Card.stories.jsx
+++ src/components/F1Card.stories.jsx
@@ -1,31 +1,6 @@
 import React from "react";
 import { F1Card } from "./F1Card"; 
-
-const verstappenData = {
-  name: "Max Verstappen",
-  team: "Red Bull Racing",
-  number: 1,
-  imageUrl:
-    "https://img.redbull.com/images/c_crop,w_3840,h_1920,x_0,y_0/c_auto,w_1200,h_630/f_auto,q_auto/redbullcom/2024/11/24/nrqoxx9as35r5ry8ashm/max-verstapen-2024-f1-world-champion-four",
-  stats: {
-    wins: 54,
-    poles: 38,
-    wdcs: 3,
-  },
-};
-
-const hamiltonData = {
-  name: "Lewis Hamilton",
-  team: "Mercedes AMG Petronas",
-  number: 44,
-  imageUrl:
-    "https://images.ps-aws.com/c?url=https%3A%2F%2Fd3cm515ijfiu6w.cloudfront.net%2Fwp-content%2Fuploads%2F2025%2F10%2F30173348%2Flewis-hamilton-ferrari-f1-2025-mexican-grand-prix-1320x742.png",
-  stats: {
-    wins: 103,
-    poles: 104,
-    wdcs: 7,
-  },
-};
+import { f1Drivers } from "../data/f1data";
 
 export default {
   title: "Components/F1Card",
@@ -45,13 +20,19 @@ export default {
 
 export const Verstappen = {
   args: {
-    driver: verstappenData,
+    driver: f1Drivers[1],
   },
 };
 
 export const Hamilton = {
   args: {
-    driver: hamiltonData,
+    driver: f1Drivers[0],
+  },
+};
+
+export const George = {
+  args: {
+    driver: f1Drivers[2],
   },
 };
 
@@ -59,4 +40,17 @@ export const NoDriver = {
   args: {
     driver: null,
   },
+};
+
+export const AllDriversGallery = {
+  render: () => (
+    <div className="flex flex-wrap items-start justify-center gap-8 p-4">
+      {f1Drivers.map((driver) => (
+        <F1Card
+          key={driver.number || driver.name}
+          driver={driver}
+        />
+      ))}
+    </div>
+  ),
 };
--- src/components/F1CardCycler.stories.jsx
+++ src/components/F1CardCycler.stories.jsx
@@ -1,32 +1,6 @@
 import React from "react";
 import { F1CardCycler } from "./F1CardCycler";
-
-const f1Drivers = [
-  {
-    name: "Lewis Hamilton",
-    team: "Mercedes AMG Petronas",
-    number: 44,
-    imageUrl:
-      "https://images.ps-aws.com/c?url=https%3A%2F%2Fd3cm515ijfiu6w.cloudfront.net%2Fwp-content%2Fuploads%2F2025%2F10%2F30173348%2Flewis-hamilton-ferrari-f1-2025-mexican-grand-prix-1320x742.png",
-    stats: { wins: 103, poles: 104, wdcs: 7 },
-  },
-  {
-    name: "Max Verstappen",
-    team: "Red Bull Racing",
-    number: 1,
-    imageUrl:
-      "https://img.redbull.com/images/c_crop,w_3840,h_1920,x_0,y_0/c_auto,w_1200,h_630/f_auto,q_auto/redbullcom/2024/11/24/nrqoxx9as35r5ry8ashm/max-verstapen-2024-f1-world-champion-four",
-    stats: { wins: 54, poles: 38, wdcs: 3 },
-  },
-  {
-    name: "George Russell",
-    team: "Mercedes AMG Petronas",
-    number: 63,
-    imageUrl:
-      "https://www.telegraph.co.uk/content/dam/formula-1/2025/03/21/TELEMMGLPICT000417235519_17425744209370_trans_NvBQzQNjv4BqqVzuuqpFlyLIwiB6NTmJwfSVWeZ_vEN7c6bHu2jJnT8.jpeg?imwidth=640",
-    stats: { wins: 1, poles: 1, wdcs: 0 },
-  },
-];
+import { f1Drivers } from "../data/f1data";
 
 export default {
   title: "Components/F1CardCycler", 
